5f1a25b836743718801e12ef957ce2c1998d8380
mike4bc4 <mchkafka@gmail.com>
2024-08-19 21:30:15 +0200

    Animation player runner update callback can now be used safely regardless of component existence.

--- Assets/Scripts/KeyframeSystem/AnimationPlayerRunner.cs
+++ Assets/Scripts/KeyframeSystem/AnimationPlayerRunner.cs
@@ -5,47 +5,65 @@ using UnityEngine;
 
 namespace KeyframeSystem
 {
-    class AnimationPlayerRunner : MonoBehaviour
+    // This class is not a MonoBehavior as we want to keep safe access to onUpdate callback.
+    // If this class would represent component, said component would be destroyed while application
+    // is closing, accessing its instance in that period of time would result in creation of new
+    // component and cause leak exception. Wrapping actual component with outer class allows to 
+    // control whether new instances can be created, and even when it can't be done, 'dummy' onUpdate 
+    // delegate still might be used, though it will not be invoked anymore.
+    class AnimationPlayerRunner
     {
-        static AnimationPlayerRunner s_Instance;
+        static Component s_Component;
+        static bool s_Quitting;
 
-        event Action m_OnUpdate;
+        class Component : MonoBehaviour
+        {
+            public event Action onUpdate;
+
+            void Update()
+            {
+                onUpdate?.Invoke();
+            }
+        }
 
         public static event Action onUpdate
         {
-            add => instance.m_OnUpdate += value;
-            remove => instance.m_OnUpdate -= value;
+            add
+            {
+                if (!s_Quitting)
+                {
+                    component.onUpdate += value;
+                }
+            }
+            remove
+            {
+                if (!s_Quitting)
+                {
+                    component.onUpdate -= value;
+                }
+            }
         }
 
-        public static AnimationPlayerRunner instance
+        static Component component
         {
             get
             {
-                if (s_Instance == null)
+                if (s_Component == null)
                 {
                     var go = new GameObject("AnimationPlayerRunner");
-                    s_Instance = go.AddComponent<AnimationPlayerRunner>();
+                    s_Component = go.AddComponent<Component>();
                     go.hideFlags |= HideFlags.NotEditable;
-                    DontDestroyOnLoad(go);
+                    GameObject.DontDestroyOnLoad(go);
                 }
 
-                return s_Instance;
+                return s_Component;
             }
         }
 
-        void Awake()
+        [RuntimeInitializeOnLoadMethod]
+        static void OnLoad()
         {
-            if (s_Instance != null && s_Instance != this)
-            {
-                Destroy(this.gameObject);
-            }
-
-            s_Instance = this;
-        }
-
-        void Update()
-        {
-            m_OnUpdate?.Invoke();
+            Application.quitting += () => s_Quitting = true;
         }
     }
 }
