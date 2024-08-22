860881b8ab3dd353522ed117b806f03cac72d5d1
mike4bc4 <mchkafka@gmail.com>
2024-08-22 12:40:57 +0200

    Removed unused board properties.

--- Assets/Scripts/Boards/BoardManager.cs
+++ Assets/Scripts/Boards/BoardManager.cs
@@ -17,35 +17,7 @@ namespace Boards
 
         List<Board> m_Boards;
         StateMachine m_StateMachine;
-        InterfaceBoard m_InterfaceBoard;
-        DialogBoard m_DialogBoard;
         ActionMapsWrapper m_ActionMapsWrapper;
-
-        public static InterfaceBoard InterfaceBoard
-        {
-            get
-            {
-                if (s_Instance.m_InterfaceBoard == null)
-                {
-                    s_Instance.m_InterfaceBoard = GetBoard<InterfaceBoard>();
-                }
-
-                return s_Instance.m_InterfaceBoard;
-            }
-        }
-
-        public static DialogBoard DialogBoard
-        {
-            get
-            {
-                if (s_Instance.m_DialogBoard == null)
-                {
-                    s_Instance.m_DialogBoard = GetBoard<DialogBoard>();
-                }
-
-                return s_Instance.m_DialogBoard;
-            }
-        }
 
         public static StateMachine StateMachine => s_Instance.m_StateMachine;
         public static ActionMapsWrapper ActionMapsWrapper => s_Instance.m_ActionMapsWrapper;
