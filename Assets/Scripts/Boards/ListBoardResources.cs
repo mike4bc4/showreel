d18e447b53d14d063894f84a33f9dfed84df6b01
mike4bc4 <mchkafka@gmail.com>
2024-09-01 19:13:20 +0200

    Added visual tree asset list board resource.

--- Assets/Scripts/Boards/ListBoardResources.cs
+++ Assets/Scripts/Boards/ListBoardResources.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using UnityEngine.Video;
 
 [CreateAssetMenu(fileName = "ListBoardResources", menuName = "Scriptable Objects/List Board Resources")]
 public class ListBoardResources : ScriptableSingleton<ListBoardResources>
 {
     [SerializeField] List<VideoClip> m_VideoClips;
+    [SerializeField] List<VisualTreeAsset> m_VisualTreeAssets;
 
     public IReadOnlyList<VideoClip> videoClips => m_VideoClips.AsReadOnly();
+    public IReadOnlyList<VisualTreeAsset> visualTreeAssets => m_VisualTreeAssets.AsReadOnly();
 }
